feec02aa3d1ac78c34c2306fbfb182e774de8d35
freya <willtheszeras@gmail.com>
2025-09-15 15:14:53 +0100

    Got regression working

--- Regressions/main.py
+++ Regressions/main.py
@@ -1,5 +1,19 @@
 import matplotlib.pyplot as plt
 import numpy as np
+#points = [(1,0), (2,0), (3,0), (4,0), (5,0), (6,0), (7,0)]
+x = [1,2,3,4,5,6,7]
+y = [0,0,0,0,0,0,0]
+m = 1
+c = 0
+E = 0
 fig, ax = plt.subplots() 
-ax.scatter([12,1,2,34,5,12,3], [1,1,1,2,2,1,5]) 
+# for i in points:
+#     ax.scatter(i[0], i[1])
+
+ax.scatter(x,y)
+
+
+m = np.sum(x - np.mean(x))
+print(m)
+
 plt.show()
