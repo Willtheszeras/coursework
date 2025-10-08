1690c53d7579c380bec9fc5ebcf3b9b5ffdef854
freya <willtheszeras@gmail.com>
2025-10-08 10:52:45 +0100

    Attempt at starting data processing

--- Sockets/Client.py
+++ Sockets/Client.py
@@ -1,13 +1,16 @@
 import socket
+#import Regressions.main as main
 
 HOST = "10.171.218.1"
 #HOST = "192.168.4.1"
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, 7777))
-    while True:
+    for i in range(100000):
         data = s.recv(1)
         if not data:
             break
         print(f"Received {data}")
+        main.inputData.append(data)
 
+#main.plot()
