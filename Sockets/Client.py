315a9bb097ed6371dfda83571f469423b0286cb0
freya <willtheszeras@gmail.com>
2025-12-09 15:08:29 +0000

    eee

--- Sockets/Client.py
+++ Sockets/Client.py
@@ -2,12 +2,12 @@ import socket
 #import Regressions.main as main
 import Translator as trans
 
-HOST = "10.171.218.1"
+HOST = "192.168.137.167"
 #HOST = "192.168.4.1"
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, 7777))
-    for i in range(1000):
+    for i in range(10000):
         data = s.recv(1)
         if not data:
             break
